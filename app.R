9ef3332e2224bf0f889334026d0be30d1fa80b2d
alex karari <karari.alexander@gmail.com>
2022-04-03 23:45:49 +0300

    Finance module added to the pipeline.

--- app.R
+++ app.R
@@ -20,12 +20,14 @@ source("src/entity/run_entity.r")
 source("src/population/run_population.r")
 source("src/supply/run_supply.R")
 source("src/vaccines/run_vaccines.r")
+source("src/finance/finance.r")
 
 base_env <- run_base()
 entity_env <- run_entity()
 pop_env <- run_population()
 supply_env <- run_supply()
 vaccines_env <- run_vaccines(entity_characteristics)
+finance_env <- run_financing()
 
 # EDA
 
---
+++ src/finance/run_finance.r
@@ -0,0 +1,23 @@
+run_financing <- function(env = .GlobalEnv) {
+    source("src/finance/finance.r")
+    
+    print(" > Starting local environment for base population")
+    print(" > Loading financing data...")
+    b_fin_funding <- load_finance_data()
+    print(" > Done.")
+
+    print(" > Transforming financing data...")
+    b_fin_fund_del_source <- transform_finance_data(
+        b_fin_funding, entity_characteristics
+    )
+    print(" > Done.")
+
+    print(" > Returning to global environment. ")
+
+    print(" > Loading financing data back to global environment...")
+    env$b_fin_funding <- b_fin_funding
+    env$b_fin_fund_del_source <- b_fin_fund_del_source
+
+    return(environment())
+}
+run_financing()
