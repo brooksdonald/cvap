25f2da2d28907d69e974a593d0512a457ab31bdf
alex karari <karari.alexander@gmail.com>
2022-04-11 19:05:24 +0300

    Modified linting issues with run financing file.

--- eda/financing/run_financing.R
+++ eda/financing/run_financing.R
@@ -1,17 +1,11 @@
 run_financing <- function(a_data, env = .GlobalEnv) {
   source("eda/financing/financing_overview.r")
-  
   print(" > Starting local environment for financing summary")
-  
   print(" > Calculating financing overview...")
   a_data <- financing_overview(a_data)
   print(" > Done.")
-  
   print(" > Loading financing summary data back to global environment...") 
   env$a_data <- a_data
   print(" > Ok.")
-  
   return(environment())
-  
 }
-run_financing(a_data)
