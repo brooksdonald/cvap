628be1c1496db3e02eb210cff7f4ae11e69a539f
alex karari <karari.alexander@gmail.com>
2022-04-11 14:19:11 +0300

    Modified run file for eda supplies. Module running ok.

--- eda/supplies/run_supplies.r
+++ eda/supplies/run_supplies.r
@@ -1,16 +1,20 @@
 # rows 2169 - 2254
 
-run_eda_supplies <- function(a_data, supply_secured, delivery_courses_doses, env = .GlobalEnv) {
+run_eda_supplies <- function(
+    a_data, supply_secured, delivery_courses_doses, env = .GlobalEnv
+) {
     source("eda/supplies/supplies_consolidate.r")
 
     print(" > Starting local environment for supplies eda")
     print(" > Supplies eda...")
-    a_data <- merge_a_data_details(a_data, supply_secured, delivery_courses_doses)
+    a_data <- merge_a_data_details(
+        a_data, supply_secured, delivery_courses_doses
+    )
     print(" > Done.")
 
     print(" > Loading eda supplies data back to global environment...")
     env$a_data <- a_data
+    print(" > Ok.")
 
     return(environment())
 }
-run_eda_supplies(a_data, supply_secured, delivery_courses_doses)
