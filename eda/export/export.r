d7190c8029216730c79846fbb735a5193429d615
brooksdonald <dj.brooks.01@gmail.com>
2022-05-04 15:23:21 +0000

    minor changes

--- eda/export/export.r
+++ eda/export/export.r
@@ -6,8 +6,7 @@ write_to_excel <- function() {
         "1_absorption_month" = d_absorption,
         "1_absorption_month_country" = combined,
         "1_stock" = combined_three,
-        "1_adm_dvr_long" = b_vxrate_amc,
-        "1_adm_long_smoohth" = b_vxrate_amc_smooth,
+        "1_adm_long_smooth" = b_vxrate_amc_smooth,
         "1_adm_all_long" = b_vxrate_pub,
         "1_delivery_doses" = supply_received_by_product,
         "1_secview" = z_temp,
@@ -29,8 +28,8 @@ write_to_excel <- function() {
         "8_ndvp_tar_cat" = e_ndvp_all,
         "9_values" = z_values
     )
-    write_xlsx(all_df, "data/output/220505_output_powerbi.xlsx")
-    write_xlsx(api, "data/output/220503_output_api.xlsx")
+    write_xlsx(all_df, "data/output/220428_output_powerbi.xlsx")
+    write_xlsx(api, "data/output/220428_output_api.xlsx")
 
 
 }
