fa8e2714578f8820932761f7f96c97f75f2d0b51
alex karari <karari.alexander@gmail.com>
2022-05-09 11:16:05 +0300

    Export process moved to app.R

--- app.R
+++ app.R
@@ -83,5 +83,34 @@ consolidate_env <- run_consolidate(a_data)
 # Export
 
 print(" > Exporting data output from pipeline to Excel Workbooks...")
+all_df <- list(
+    "0_base_data" = a_data,
+    "1_absorption_month" = d_absorption,
+    "1_absorption_month_country" = combined,
+    "1_stock" = combined_three,
+    "1_adm_long_smooth" = b_vxrate_amc_smooth,
+    "1_adm_all_long" = b_vxrate_pub,
+    "1_delivery_doses" = supply_received_by_product,
+    "1_secview" = z_temp,
+    "1_secview_lm" = z_temp_lm,
+    "1_secview_all" = z_secview_long,
+    "1_funding_source" = b_fin_fund_del_source,
+    "2_dvr_perchange_count" = f_dvr_change_count,
+    "2_cov_change_count" = f_cov_change_count,
+    "2_dvr_perchange_count_af" = f_dvr_change_count_af,
+    "2_cov_change_count_af" = f_cov_change_count_af,
+    "8_dvr_cat" = e_vrcat_all,
+    "8_dvr_lm_trend" = e_trend_all,
+    "8_tarpast_cat" = e_tar_past_all,
+    "8_curtar_cat" = e_tar_cur_all,
+    "8_curtar_scale_cat" = e_tar_cur_scale_all,
+    "8_booster_status" = e_booster_all,
+    "8_secdelpu_cat" = e_secdelpu_all,
+    "8_cov_cat" = e_cov_all,
+    "8_ndvp_tar_cat" = e_ndvp_all,
+    "9_values" = z_values
+)
+write_xlsx(all_df, "data/output/test_powerbi.xlsx")
+write_xlsx(api, "data/output/test_output_api.xlsx")
 
-export_env <- run_export()
+print(" > Output exported successfully!")
