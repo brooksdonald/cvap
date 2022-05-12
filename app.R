a4b53e359518d1a95a5207b4fba1523a5460d9f7
alex karari <karari.alexander@gmail.com>
2022-05-12 09:54:02 +0300

    Updated the sec date to 05/05/2022

--- app.R
+++ app.R
@@ -28,7 +28,7 @@ source("helpers/transformations.r")
 # STATIC DATES
 .GlobalEnv$refresh_date <- as.Date("2022-05-05")
 .GlobalEnv$t70_deadline <- as.Date("2022-06-30")
-.GlobalEnv$dataset_date <- "2022-04-28" # dataset_date is passed to sec_date
+.GlobalEnv$dataset_date <- "2022-05-05" # dataset_date is passed to sec_date
 .GlobalEnv$del_date <- as.Date("2022-05-05")
 
 # ETL
