6ad53cd04d55e94d01d9e0e50c64434427559e0f
alex karari <karari.alexander@gmail.com>
2022-05-18 22:34:52 +0300

    Output dates on excel files updated to reflect recent output date

--- app.R
+++ app.R
@@ -116,7 +116,7 @@ all_df <- list(
     "8_ndvp_tar_cat" = e_ndvp_all,
     "9_values" = z_values
 )
-# write_xlsx(all_df, "data/output/220513_output_powerbi.xlsx")
-# write_xlsx(api, "data/output/220513_output_api.xlsx")
+write_xlsx(all_df, "data/output/220518_output_powerbi.xlsx")
+write_xlsx(api, "data/output/220518_output_api.xlsx")
 
 print(" > Output exported to Excel successfully!")
