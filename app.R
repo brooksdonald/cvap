5316669770eff0d6d4d3e4b1ec4dad22c313c9bf
alex karari <karari.alexander@gmail.com>
2022-06-10 10:23:18 +0300

    Global dates changed for week of 220606

--- app.R
+++ app.R
@@ -26,10 +26,10 @@ source("helpers/joins.r")
 source("helpers/transformations.r")
 
 # STATIC DATES
-.GlobalEnv$refresh_date <- as.Date("2022-06-02")
+.GlobalEnv$refresh_date <- as.Date("2022-06-09")
 .GlobalEnv$t70_deadline <- as.Date("2022-06-30")
-.GlobalEnv$dataset_date <- "2022-05-26" # dataset_date is passed to sec_date
-.GlobalEnv$del_date <- as.Date("2022-05-30")
+.GlobalEnv$dataset_date <- "2022-06-02" # dataset_date is passed to sec_date
+.GlobalEnv$del_date <- as.Date("2022-06-09")
 
 # ETL
 
@@ -116,7 +116,7 @@ all_df <- list(
     "8_ndvp_tar_cat" = e_ndvp_all,
     "9_values" = z_values
 )
-# write_xlsx(all_df, "data/output/220602_output_powerbi.xlsx")
-# write_xlsx(api, "data/output/220602_output_api.xlsx")
+# write_xlsx(all_df, "data/output/220609_output_powerbi.xlsx")
+# write_xlsx(api, "data/output/220609_output_api.xlsx")
 
 print(" > Output exported to Excel successfully!")
