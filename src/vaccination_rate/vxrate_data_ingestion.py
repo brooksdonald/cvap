aa5be39742b2ab76fd1a8fad9fda9fb3b0a1b8c5
alexkarari <karari.alexander@gmail.com>
2022-06-14 15:31:25 +0300

    vxrate data ingestion file done and tested.

---
+++ src/vaccination_rate/vxrate_data_ingestion.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Country Vx Throughput Analysis - Data
+#  
+# **Note:** Gets data for the WHO/BMGF/Gavi Vx Throughput task force
+# 
+# * Source:
+# 
+# * Libraries: 
+#   - Python
+# 
+# * Built by: Jeremy Cooper
+# * Current owner: Jeremy Cooper
+# * Initial Build Date: 06/14/2021
+# * Latest Build Date: 06/14/2021
+
+# Import required libraries
+from datetime import date
+from urllib.request import Request, urlopen
+import urllib
+import json
+import pandas as pd
+import numpy as np
+import datetime
+from urllib.error import HTTPError
+
+# ### Get Data
+print(" > Gettig data...")
+url1 = 'https://frontdoor-l4uikgap6gz3m.azurefd.net/NCOV/VAC_REP_COUNTS'
+url2 = 'https://frontdoor-l4uikgap6gz3m.azurefd.net/NCOV/VAC_REP_COUNTS_EUR'
+print(" > Done...")
+
+# 2 data sources for Throughput, one for Europe and one for ROW
+# read in separately, clean, and then bind together
+print(" > read in data sources separately, clean, and then bind together...")
+response = urlopen(url1)
+data_json = json.loads(response.read())
+df1 = pd.DataFrame(data_json["value"])
+df1 = df1[['COUNTRY_FK', 'AS_OF_DATE', 'DOSES_ADMINISTERED', 'PERSONS_VACCINATED_ONE_PLUS_DOSE', 'PERSONS_VACCINATED_FULL', 'PERSONS_BOOSTER_ADD_DOSE', 'SOURCE']]
+df1 = df1[df1['COUNTRY_FK']!='None']
+df1.columns = ['entity', 'date', 'total_doses', 'at_least_one_dose', 'fully_vaccinated', 'persons_booster_add_dose', 'source']
+df1['country_name'] = df1['entity'].str.title()
+df1 = pd.DataFrame(df1).drop_duplicates()
+print(" > Done...")
+print(" > printing df")
+print(df1)
+print(" > Done...")
+
+
+try:
+  response = urlopen(url2)
+  data_json = json.loads(response.read())
+except HTTPError as e:
+  data_json = json.loads(e.read())
+
+df2 = pd.DataFrame(data_json["value"])
+df2 = df2.astype(str)
+df2 = df2[['COUNTRY_FK', 'AS_OF_DATE', 'DOSES_ADMINISTERED', 'PERSONS_VACCINATED_ONE_PLUS_DOSE', 'PERSONS_VACCINATED_FULL', 'PERSONS_BOOSTER_ADD_DOSE', 'SOURCE']]
+df2 = df2[df2['COUNTRY_FK']!='None']
+df2.columns = ['entity', 'date', 'total_doses', 'at_least_one_dose', 'fully_vaccinated', 'persons_booster_add_dose', 'source']
+df2['country_name'] = df2['entity'].str.title()
+df2 = pd.DataFrame(df2).drop_duplicates()
+print(df2)
+
+print(" > Joining 2 df to one...")
+df3=pd.concat([df1,df2], ignore_index = True)
+print(" > Done...")
+
+# Save df3 to variable df_data
+df_data = pd.DataFrame(df3).drop_duplicates()
+
+
+# # clean Excel columns, replace None and nan
+# col_names = df_data.columns
+# print(col_names)
+
+# for name in col_names:
+#     df_data = df_data.withColumn(name, when(~col(name).isin(['None', 'nan']), trim(col(name))).otherwise(None)) 
+
+# manaully fix some data date issues
+print(" > Manually fixing data date issus...")
+df_data.loc[np.logical_and(df_data.loc[:,'entity'] == 'ANGUILLA' , df_data.loc[:,"date"] == "2001-02-26"), "date"] = "2021-02-26"
+df_data.loc[np.logical_and(df_data.loc[:,'entity'] == 'TRINIDAD AND TOBAGO', df_data.loc[:,"date"] == "2001-02-26"), "date"] = "2021-02-26"
+df_data.loc[np.logical_and(df_data.loc[:,'entity'] == 'PAKISTAN', df_data.loc[:,"date"] == "2010-03-05"), "date"] = "2021-03-05"
+df_data.loc[np.logical_and(df_data.loc[:,'entity'] == 'CANADA', df_data.loc[:,"date"] == "2020-01-29"), "date"] = "2021-01-29"
+df_data.loc[np.logical_and(df_data.loc[:,'entity'] == 'LIBYA', df_data.loc[:,"date"] == "1900-05-18"), "date"] = "2021-05-18"
+df_data.loc[np.logical_and(df_data.loc[:,'entity'] == 'LIBYA', df_data.loc[:,"date"] == "1900-05-20"), "date"] = "2021-05-20"
+df_data.loc[np.logical_and(df_data.loc[:,'entity'] == 'LIBYA', df_data.loc[:,"date"] == "1900-05-25"), "date"] = "2021-05-25"
+df_data.loc[np.logical_and(df_data.loc[:,'entity'] == 'LIBYA', df_data.loc[:,"date"] == "1900-05-26"), "date"] = "2021-05-26"
+df_data.loc[np.logical_and(df_data.loc[:,'entity'] == 'BRUNEI DARUSSALAM', df_data.loc[:,"date"] == "2021-03-20"), "date"] = "2021-04-20"
+df_data.loc[np.logical_and(df_data.loc[:,'entity'] == 'LIBYA', df_data.loc[:,"date"] == "2021-01-08"), "date"] = "2022-01-08"
+df_data.loc[np.logical_and(df_data.loc[:,'entity'] == 'MOROCCO', df_data.loc[:,"date"] == "2021-01-04"), "date"] = "2021-01-04"
+df_data.loc[np.logical_and(df_data.loc[:,'entity'] == 'MOROCCO', df_data.loc[:,"date"] == "2021-01-08"), "date"] = "2021-01-08"
+print(" > Done...")
+
+# print(df_data.loc[df_data.loc[:,"entity"] == "LIBYA","date"])
+# print(df_data.loc[df_data.loc[:,"date"] == "2021-05-18"])
+
+# date stamp dataset
+print(" > Getting the stamp dataset...")
+df_data["date_accessed"] = datetime.date.today()
+print(" > Done.")
+
+# Save to CSV file
+print(" > Saving output to csv file...")
+df_data.to_csv("data/output/dose_adm.csv", index=False)
+print(" > Done.")
