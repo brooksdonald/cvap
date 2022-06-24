3b0b43790ca95d54553e395f7b3512ff19f1759e
Sebastian Dodt <77883431+sebdodt@users.noreply.github.com>
2022-06-24 14:55:09 +0300

    added functions to 04_Vx

--- src/vaccination_rate/vxrate_data_fixes.py
+++ src/vaccination_rate/vxrate_data_fixes.py
@@ -1,37 +1,63 @@
 import pandas as pd
 
-iso_mapping = pd.read_csv("data/_input/supply_data/iso_mapping.csv")
-raw = pd.read_csv("data/_input/supply_data/analysis_vx_throughput_data.csv")
-who = pd.read_csv("data/_input/supply_data/analysis_vx_throughput_output_daily.csv")
+def import_data():
+    print(' > Importing data for data_fixes.py ...')
+    iso_mapping = pd.read_csv("data/_input/supply_data/iso_mapping.csv")
+    raw = pd.read_csv("data/_input/supply_data/analysis_vx_throughput_data.csv")
+    who = pd.read_csv("data/_input/supply_data/analysis_vx_throughput_output_daily.csv")
+    return iso_mapping, raw, who
 
-df1 = raw.copy()
-df1 = df1.loc[((~(df1['total_doses'].isna())) & (df1['total_doses'] > 0)), :]
-df1 = df1[['country_name', 'date', 'total_doses', 'at_least_one_dose', 'fully_vaccinated', 'date_accessed']]
-df1['date'] = pd.to_datetime(df1['date'], format = '%Y-%m-%d')
-df1.drop_duplicates(inplace = True)
-df1 = df1.merge(iso_mapping, on = 'country_name', how = 'left')
-df1.loc[df1['country_name'] == 'Bonaire, Sint Eustatius And Saba/Saba', 'iso_code'] = 'BES1'
-df1.loc[df1['country_name'] == 'Bonaire, Sint Eustatius And Saba/Sint Eustatius', 'iso_code'] = 'BES1'
-df1.loc[df1['country_name'] == 'Bonaire, Sint Eustatius And Saba', 'iso_code'] = 'BES2'
-df1.loc[df1['country_name'] == 'Bonaire, Sint Eustatius And Saba/Bonaire', 'iso_code'] = 'XAA'
-df1.loc[df1['country_name'] == 'Occupied Palestinian Territory, Including East Jerusalem', 'iso_code'] = 'PSE'
-df1.loc[df1['country_name'] == 'Sint Maarten', 'iso_code'] = 'SXM'
-df1.loc[df1['country_name'] == 'Wallis And Futuna', 'iso_code'] = 'WLF'
-df1.loc[df1['country_name'] == 'Pitcairn Islands', 'iso_code'] = 'PCN'
-df1.loc[df1['country_name'] == 'Northern Mariana Islands (Commonwealth Of The)', 'iso_code'] = 'MNP'
-df1.loc[df1['country_name'] == 'The United Kingdom', 'iso_code'] = 'GBR'
-df1.loc[df1['country_name'] == 'Turkey', 'iso_code'] = 'TUR'
-df1.loc[df1['country_name'] == 'Kosovo', 'iso_code'] = 'KOS'
 
-df1.fillna(0, inplace = True)
+def clean_country_names(raw, iso_mapping):
+    print(" > Matching ambiguous country names...")
+    df1 = raw.copy()
+    df1 = df1.loc[((~(df1['total_doses'].isna())) & (df1['total_doses'] > 0)), :]
+    df1 = df1[['country_name', 'date', 'total_doses', 'at_least_one_dose', 'fully_vaccinated', 'date_accessed']]
+    df1['date'] = pd.to_datetime(df1['date'], format = '%Y-%m-%d')
+    df1.drop_duplicates(inplace = True)
+    df1 = df1.merge(iso_mapping, on = 'country_name', how = 'left')
+    df1.loc[df1['country_name'] == 'Bonaire, Sint Eustatius And Saba/Saba', 'iso_code'] = 'BES1'
+    df1.loc[df1['country_name'] == 'Bonaire, Sint Eustatius And Saba/Sint Eustatius', 'iso_code'] = 'BES1'
+    df1.loc[df1['country_name'] == 'Bonaire, Sint Eustatius And Saba', 'iso_code'] = 'BES2'
+    df1.loc[df1['country_name'] == 'Bonaire, Sint Eustatius And Saba/Bonaire', 'iso_code'] = 'XAA'
+    df1.loc[df1['country_name'] == 'Occupied Palestinian Territory, Including East Jerusalem', 'iso_code'] = 'PSE'
+    df1.loc[df1['country_name'] == 'Sint Maarten', 'iso_code'] = 'SXM'
+    df1.loc[df1['country_name'] == 'Wallis And Futuna', 'iso_code'] = 'WLF'
+    df1.loc[df1['country_name'] == 'Pitcairn Islands', 'iso_code'] = 'PCN'
+    df1.loc[df1['country_name'] == 'Northern Mariana Islands (Commonwealth Of The)', 'iso_code'] = 'MNP'
+    df1.loc[df1['country_name'] == 'The United Kingdom', 'iso_code'] = 'GBR'
+    df1.loc[df1['country_name'] == 'Turkey', 'iso_code'] = 'TUR'
+    df1.loc[df1['country_name'] == 'Kosovo', 'iso_code'] = 'KOS'
+    df1.fillna(0, inplace = True)
+    return df1
 
-who1 = who[['iso_code', 'date', 'total_doses', 'at_least_one_dose', 'fully_vaccinated']]
-who1['date'] = pd.to_datetime(who1['date'], format = '%Y-%m-%d')
-who1['check'] = 1
-who1.fillna(0, inplace = True)
 
-df2 = df1.merge(who1, on = ['iso_code', 'date', 'total_doses', 'at_least_one_dose', 'fully_vaccinated'], how = 'left')
-df2 = df2.loc[df2['check'].isna(), :]
-df2 = df2[['iso_code', 'country_name', 'date', 'total_doses', 'at_least_one_dose', 'fully_vaccinated', 'date_accessed']]
+def clean_who_data(who):
+    print(" > Transforming WHO data before merge...")
+    who1 = who[['iso_code', 'date', 'total_doses', 'at_least_one_dose', 'fully_vaccinated']]
+    who1['date'] = pd.to_datetime(who1['date'], format = '%Y-%m-%d')
+    who1['check'] = 1
+    who1.fillna(0, inplace = True)
+    return who1
 
-df2.to_csv("data/_input/supply_data/analysis_vx_data_fixes.csv", index=False)
+
+def merge_dataframes(df1, who1):
+    print(" > Merging dataframes for data fixes...")
+    df2 = df1.merge(who1, on = ['iso_code', 'date', 'total_doses', 'at_least_one_dose', 'fully_vaccinated'], how = 'left')
+    df2 = df2.loc[df2['check'].isna(), :]
+    df2 = df2[['iso_code', 'country_name', 'date', 'total_doses', 'at_least_one_dose', 'fully_vaccinated', 'date_accessed']]
+    return df2
+
+
+def export_dataframe(df2):
+    print(" > Saving analysis_vx_data_fixes to csv file...")
+    df2.to_csv("data/_input/supply_data/analysis_vx_data_fixes.csv", index=False)
+    print(" > Done")
+
+
+iso_mapping, raw, who = import_data()
+df1 = clean_country_names(raw, iso_mapping)
+who1 = clean_who_data(who)
+df2 = merge_dataframes(df1, who1)
+export_dataframe(df2)
+print(" > Closing Python Environment")
