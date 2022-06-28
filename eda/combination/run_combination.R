ebb995fd58c0e3637f36ea3e3a4debed70519423
Sebastian Dodt <77883431+sebdodt@users.noreply.github.com>
2022-06-28 16:53:37 +0300

    passed global variables back through the functions

--- eda/combination/run_combination.R
+++ eda/combination/run_combination.R
@@ -2,10 +2,17 @@ run_combination <- function(a_data, env = .GlobalEnv) {
   source("eda/combination/combination.r")
   print(" > Starting local environment for supply & administation summary")
   print(" > Overlaying supply and administration data...")
-  a_data <- supply_admin_summary(a_data)
+  datalist <- supply_admin_summary(a_data)
+  a_data <- datalist$a_data
+  z_temp <- datalist$z_temp
+  z_temp_lm <- datalist$z_temp_lm
+  z_secview_long <- datalist$z_secview_long
   print(" > Done.")
   print(" > Loading supply & administration data back to global environment...") 
   env$a_data <- a_data
+  env$z_temp <- z_temp
+  env$z_temp_lm <- z_temp_lm
+  env$z_secview_long <- z_secview_long
   print(" > Ok.")
   return(environment())
 }
