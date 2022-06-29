d9efea5f247dcb73f04e83227558bb24673ffd49
Sebastian Dodt <77883431+sebdodt@users.noreply.github.com>
2022-06-29 09:32:27 +0300

    wrapped functions in main() if clause

--- src/vaccination_rate/vxrate_supply_data.py
+++ src/vaccination_rate/vxrate_supply_data.py
@@ -70,9 +70,10 @@ def export_data(df_uti):
     return df_uti
 
 
-df = import_data()
-df = validate_and_filter(df)
-df_uti = create_date_column(df)
-df_uti = aggregate_util_data(df_uti)
-df_uti = add_timestamp(df_uti)
-export_data(df_uti)
+if __name__ == '__main__':
+    df = import_data()
+    df = validate_and_filter(df)
+    df_uti = create_date_column(df)
+    df_uti = aggregate_util_data(df_uti)
+    df_uti = add_timestamp(df_uti)
+    export_data(df_uti)
