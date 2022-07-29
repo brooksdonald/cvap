2eacc231d6366e9ce20eb9a9c04ea6775da12162
sebdodt <mats.dodt@googlemail.com>
2022-07-29 15:01:14 +0300

    deleting obsolete naming for removed supply output in run_dvr()

--- src/dvr/run_dvr.r
+++ src/dvr/run_dvr.r
@@ -16,24 +16,23 @@ run_dvr <- function(adm_api, auto_cleaning, headers, refresh_api) {
         print(" > Defining storage locations...")
         folder <- "_input/dvr_data"
         name_00 <- "analysis_vx_throughput_data.csv"
-        name_01 <- "analysis_vx_throughput_supply.csv"
-        name_02 <- "analysis_vx_throughput_data_cleaned.csv"
-        name_03 <- "analysis_vx_throughput_output_daily.csv"
-        name_04 <- "analysis_vx_data_fixes.csv"
+        name_01 <- "analysis_vx_throughput_data_cleaned.csv"
+        name_02 <- "analysis_vx_throughput_output_daily.csv"
+        name_03 <- "analysis_vx_data_fixes.csv"
 
         print(" > Opening Python Environment...")
         source_python("src/dvr/dvr_data_ingestion.py")
         throughput_data <- main(folder, name_00, refresh_api)
 
         source_python("src/dvr/dvr_data_cleaning.py")
-        cleaned_data <- main(auto_cleaning, throughput_data, folder, name_02)
+        cleaned_data <- main(auto_cleaning, throughput_data, folder, name_01)
 
         source_python("src/dvr/dvr_output_daily.py")
-        dvr_data <- main(cleaned_data, folder, name_03,
+        dvr_data <- main(cleaned_data, folder, name_02,
             refresh_api, auto_cleaning)
 
         source_python("src/dvr/dvr_data_fixes.py")
-        main(throughput_data, dvr_data, folder, name_04)
+        main(throughput_data, dvr_data, folder, name_03)
 
     } else {
         print(" > adm_api == FALSE")
