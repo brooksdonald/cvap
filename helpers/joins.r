e92078ee433dcfa7471f1e3729242ca8db74ac9c
sebdodt <mats.dodt@googlemail.com>
2022-08-09 08:55:50 +0300

    refactored load_secured_expected() in supply_timeseries

--- helpers/joins.r
+++ helpers/joins.r
@@ -12,3 +12,22 @@ helper_join_dataframe_list <- function(l, join_by, allx = TRUE, ally = FALSE) {
 
     return(join)
 }
+
+helper_load_list_of_files <- function(files, sheet_name, date_format) {
+    df_list <- list()
+    for (i in seq_along(files)) {
+        length_of_date_string <- nchar(
+            format(as.Date("2000-01-01"), date_format))
+        date <- as.Date(
+            substr(sub(".*/", "", files[i]), 1, length_of_date_string),
+            date_format)
+        month <- substr(date, 1, 7)
+        df <- data.frame(
+            read_excel(
+                files[i],
+                sheet = sheet_name)) %>%
+            mutate(month_name = month)
+        df_list[[i]] <- df
+    }
+    return(df_list)
+}
