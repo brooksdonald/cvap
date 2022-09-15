a31be1c7d77c98685bedadbeee686291501ad259
alex karari <karari.alexander@gmail.com>
2022-09-15 14:06:16 +0300

    Supply time series set to false for week of 220912

--- app.R
+++ app.R
@@ -1,6 +1,6 @@
 
 # SET WD
-setwd("C:/Users/brooksd/OneDrive - World Health Organization/Documents/GitHub/covid19_vaccination_analysis") #Donald
+# setwd("C:/Users/brooksd/OneDrive - World Health Organization/Documents/GitHub/covid19_vaccination_analysis") #Donald
 # setwd("C:/Users/rafae/Desktop/covid19_vaccination_analysis") #Rafael
 
 # CLEAR ENVIRONMENT
@@ -35,7 +35,7 @@ lapply(lib, library, character.only = TRUE)
 .GlobalEnv$auto_cleaning <- TRUE # set to FALSE for no automised cleaning
 .GlobalEnv$adm_api <- TRUE # DO NOT TOUCH. Set to FALSE to use base_dvr_current.xlsx
 .GlobalEnv$refresh_api <- TRUE # set to FALSE to use last API call
-.GlobalEnv$refresh_supply_timeseries <- TRUE # FALSE reads ../static/supply.xlsx Unless stated otherwise by Donald 
+.GlobalEnv$refresh_supply_timeseries <- FALSE # FALSE reads ../static/supply.xlsx Unless stated otherwise by Donald 
 
 # HELPERS
 
