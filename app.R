83cc7bee06f9f839bb7632115f12750e332ec6eb
alexkarari <karari.alexander@gmail.com>
2022-09-22 10:43:40 +0300

    Static dates and output files modified to reflect changes of week of 220922

--- app.R
+++ app.R
@@ -28,13 +28,13 @@ lapply(lib, library, character.only = TRUE)
 
 # STATIC VARIABLES
 
-.GlobalEnv$refresh_date <- as.Date("2022-09-15")
+.GlobalEnv$refresh_date <- as.Date("2022-09-22")
 .GlobalEnv$sec_date <- as.Date("2022-08-31")
-.GlobalEnv$del_date <- as.Date("2022-09-13")
+.GlobalEnv$del_date <- as.Date("2022-09-20")
 .GlobalEnv$t70_deadline <- as.Date("2022-12-31")
 .GlobalEnv$auto_cleaning <- TRUE # set to FALSE for no automised cleaning
 .GlobalEnv$adm_api <- TRUE # DO NOT TOUCH. Set to FALSE to use base_dvr_current.xlsx
-.GlobalEnv$refresh_api <- FALSE # set to FALSE to use last API call
+.GlobalEnv$refresh_api <- TRUE # set to FALSE to use last API call
 .GlobalEnv$refresh_supply_timeseries <- FALSE # FALSE reads ../static/supply.xlsx Unless stated otherwise by Donald 
 
 # HELPERS
@@ -161,8 +161,8 @@ all_df <- list(
     "1_fund_cds_long" = finance_env$base_fin_cds_red
 )
 
-# write_xlsx(all_df, "data/output/220915_output_powerbi.xlsx")
-# write_xlsx(financing_env$api, "data/output/220915_output_api.xlsx")
-# write_xlsx(all_df, "data/output/output_master.xlsx")
+write_xlsx(all_df, "data/output/220922_output_powerbi.xlsx")
+write_xlsx(financing_env$api, "data/output/220922_output_api.xlsx")
+write_xlsx(all_df, "data/output/output_master.xlsx")
 
 print(" > Output exported to Excel successfully!")
