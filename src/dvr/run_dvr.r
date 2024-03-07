e80110f7f6a5016437b0b84fc63a6c21394d135e
Donald Brooks <93133199+brooksdonald@users.noreply.github.com>
2024-03-07 18:28:12 +0100

    Create run_dvr.r

---
+++ src/dvr/run_dvr.r
@@ -0,0 +1,33 @@
+
+run_dvr <- function(adm_api, auto_cleaning, headers, refresh_api) {
+    print("> Checking if all Python libraries are available...")
+    python_packages <- c("datetime", "io", "json", "matplotlib", "numpy", 
+                         "openpyxl", "os", "pandas", "pickle", "seaborn", 
+                         "urllib", "warnings", "sys")
+    
+    for (module in python_packages) {
+      if (!py_module_available(module)) {
+        print(paste0(" > ", module, " is not available. Installing now..."))
+        py_install(module)
+      }
+    }
+  print(" > All Python libraries loaded.")
+  
+  print("> Loading src/dvr module scripts...")
+  source("src/dvr/dvr_data_ingestion.r")
+  print("> Loading general administration data...")
+  throughput_data <- main(refresh_api)
+  print("> Done.")
+  
+  source_python("src/dvr/dvr_data_cleaning.py")
+  print("> Cleaning general administration data...")
+  cleaned_data <- main(auto_cleaning, throughput_data)
+  print("> Done.")
+  
+  source_python("src/dvr/dvr_output_daily.py")
+  print("> Interpolating cleaned general administration data...")
+  dvr_data <- main(cleaned_data, refresh_api, auto_cleaning)
+  print("> Done.")
+  
+  return(environment())
+}
